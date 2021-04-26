8f0ad65e84169ff2583296078d1eca76802654af
langdorp <matthieu.langdorph@gmail.com>
2021-04-26 23:50:47 +0200

    Add first hard cmd for client.c

--- src/client.c
+++ src/client.c
@@ -9,23 +9,41 @@
 #include <string.h>
 #include <unistd.h>
 
+#define BUFSIZE 512
 
 
 int main(int argc, char *argv[]){
-    int port_diff, port_multicast;
+    int port_diff, port_multicast, port_manager;
     char *adresse_diff;
     char *adresse_multicast;
-    if(argc != 5 && argc!=3){
+    char *adresse_manager;
+    if(argc!=4 && argc!=3){
         printf("Erreur : Il n'y a pas le bon nombre d'arguments...\n");
         printf("Rappel des formats clients :\n\t./client adresse_diff port_diff\n\t./client adresse_multicast port_multicast\n\t./client addresse_multicast port_multicast adresse_diff port_diff");
         return EXIT_FAILURE;
     }
+    if(argc==4 && strcmp(argv[1], "d")==0){
+        strcpy(adresse_diff, argv[2]);
+        port_diff = atoi(argv[3]);
+    }
+    else if(argc==4 && strcmp(argv[1], "m")==0){
+        strcpy(adresse_multicast, argv[2]);
+        port_multicast = atoi(argv[3]);
+    }
+    else {
+        int i, j;
+        if(argc==3){ i = 1; j = 2; }
+        else { i = 2; j = 3; }
+        strcpy(adresse_manager, argv[i]);
+        port_manager = atoi(argv[j]);
+    }
+    
 
     struct sockaddr_in adr_sock;
     adr_sock.sin_family = AF_INET;
-    adr_sock.sin_port = htons(port_diff);
-    if(inet_aton(adresse_diff, &adr_sock.sin_addr) == 0){
-        printf("Adress unvalid...");
+    adr_sock.sin_port = htons(port_manager);
+    if(inet_aton(adresse_manager, &adr_sock.sin_addr) == 0){
+        printf("Adress unvalid...\n");
         return EXIT_FAILURE;
     }
 
@@ -34,7 +52,40 @@ int main(int argc, char *argv[]){
         printf("Erreur lors de la création de la socket...\n");
         return EXIT_FAILURE;
     }
+
+    int r  = connect(sock, (struct sockaddr *)&adr_sock, sizeof(struct sockaddr_in));
+    if(r == -1){
+        printf("Erreur connect\n");
+        return EXIT_FAILURE;
+    }
+
+    char *mess = malloc(10);
+    create_message(mess, LIST);
+    send(sock, mess, msglen(LIST), 0);
+
+    char buf[BUFSIZE];
+    r = recv(sock, buf, BUFSIZE-1, 0);
+    if(r <= 0){
+        perror("recv");
+        return EXIT_FAILURE;
+    }
+    buf[r] = '\0';
     
+
+    char * type = get_msg_type(buf);
+    int num_diff;
+    switch(type){
+        case LINB : 
+            num_diff = atoi(buf[5]);
+        default :
+            printf("ERREUR TYPE MESSAGE\n");
+            return EXIT_FAILURE;
+    }
+
+    for(int  i = 0; i < num_diff; i++){
+        // recv ITEM
+    }
+
 
 
     return 0;
